fc541109c719131da2897c7169d0fc801812ea32
d0lbangi <d0lbangi13@naver.com>
2023-03-08 06:29:56 +0900

    More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
 // 컨트롤러의 이름이 뭐냐고 물었을 때 입력한 이름의 값이 특별하게 취급된다.
@@ -10,13 +10,20 @@ export class MoviesController {
   getAll() {
     return `This will return all movies`;
   }
+  @Get('search')
+  //search부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단한다.
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+  // id가 위에서 2번째에 위치하면 다른 get들은 작동하지 않는다.
   @Get('/:id')
   getOne(@Param('id') MovieId: string) {
     return `This will return one movies the id: ${MovieId}`;
   }
   @Post()
-  create() {
-    return `This will create a movie`;
+  // 아래 Decorator는 movieData안의 리퀘스트의 body를 가져오기 위해 쓴 것이다.
+  create(@Body() movieData) {
+    return movieData;
   }
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
@@ -25,7 +32,12 @@ export class MoviesController {
   // @Put은 모든 리소스를 업데이트 한다.
   // @Patch는 리소스의 일부분만 업데이트 해준다.
   @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  // 필요한 parameter를 직접 요청하지 않으면, 아무것도 제공되지 않는다.
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    // 업데이트할 movie의 id랑 우리가 보낼 데이터의 오브젝트를 리턴할 것이다.
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
   }
 }
