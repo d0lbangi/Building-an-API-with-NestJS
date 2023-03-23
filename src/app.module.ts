6e690189e3f4259b0bb8d7348f8cd84ad05c3549
d0lbangi <d0lbangi13@naver.com>
2023-03-23 14:32:59 +0900

    FEAT: Movies Service part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 // 해당 함수는 데코레이터라고 부른다. 이는 클래스에 함수 기능을 추가할 수 있다.
 // 클래스 위의 함수이고, 클래스를 위해 움직인다.
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,3 +1,5 @@
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
@@ -5,38 +7,45 @@ import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestj
 // 'movies' 부분이 컨트롤러를 위한 url을 만든다. 라우터이다.
 // @Controller()부분이 url의 Entry Point(엔트리 포인트)를 컨트롤한다.
 export class MoviesController {
+  // constructor는 moviesService라는 클래스를 가진다
+  constructor(private readonly MoviesService: MoviesService) {}
+
   @Get()
   // express.js 어플리케이션을 쓸 때의 그 라우터들이 된다.
-  getAll() {
-    return `This will return all movies`;
+  getAll(): Movie[] {
+    // getAll은 Movie Array를 리턴할 것이다.
+    return this.MoviesService.getAll();
   }
-  @Get('search')
-  //search부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단한다.
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after: ${searchingYear}`;
-  }
+  // @Get('search')
+  // //search부분이 get보다 밑에 있으면 NestJS는 search를 id로 판단한다.
+  // //가져오게 되는 변수와 리턴값을 한눈에 볼 수 있다.
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear}`;
+  // }
   // id가 위에서 2번째에 위치하면 다른 get들은 작동하지 않는다.
-  @Get('/:id')
-  getOne(@Param('id') MovieId: string) {
-    return `This will return one movies the id: ${MovieId}`;
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return this.MoviesService.getOne(movieId);
   }
+
   @Post()
-  // 아래 Decorator는 movieData안의 리퀘스트의 body를 가져오기 위해 쓴 것이다.
+  // 아래 Decorator(@Body)는 movieData안의 리퀘스트의 body를 가져오기 위해 쓴 것이다.
   create(@Body() movieData) {
-    return movieData;
+    return this.MoviesService.create(movieData);
   }
-  @Delete('/:id')
+
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.MoviesService.deleteOne(movieId);
   }
   // @Put은 모든 리소스를 업데이트 한다.
   // @Patch는 리소스의 일부분만 업데이트 해준다.
-  @Patch('/:id')
-  // 필요한 parameter를 직접 요청하지 않으면, 아무것도 제공되지 않는다.
+  @Patch(':id')
+  // 필요한 parameter를 직접 요청하지 않으면, 아무것도 제공되지 않는다. 리소스의 일부분만 업데이트 할 경우
   patch(@Param('id') movieId: string, @Body() updateData) {
     // 업데이트할 movie의 id랑 우리가 보낼 데이터의 오브젝트를 리턴할 것이다.
     return {
-      updateMovie: movieId,
+      updatedMovie: movieId,
       ...updateData,
     };
   }
