5374eb06c3a84a71ae9afb0445ca34d5a502bf83
d0lbangi <d0lbangi13@naver.com>
2023-03-23 17:23:00 +0900

    FEAT:Testinng getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,23 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
-  // movies.service를 테스팅하는 파일이다.
   let service: MoviesService;
+  expect(service).toBeDefined();
+});
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MoviesService],
-    }).compile();
-
-    service = module.get<MoviesService>(MoviesService);
+it('should be 4', () => {
+  expect(2 + 3).toEqual(5);
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
 
-  it('should be 4', () => {
-    expect(2 + 2).toEqual(4);
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
